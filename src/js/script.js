e7b6216150f30984afcab737aca558d814091af8
Nika <vva12369@gmail.com>
2022-02-10 18:18:48 +0600

    add new file

--- src/js/script.js
+++ src/js/script.js
@@ -6,24 +6,3 @@ const userInfo = {
     "58": 'puk'
 }
 console.log(userInfo[58]);
-
-
-const userInfo = {
-    name: "Вася",
-    age: 30,
-    "58": 'puk'
-}
-console.log(userInfo[58]);
-
-const userInfo = {
-    name: "Вася",
-    age: 30,
-    "58": 'puk'
-}
-console.log(userInfo[58]);
-const userInfo = {
-    name: "Вася",
-    age: 30,
-    "58": 'puk'
-}
-console.log(userInfo[58]);
