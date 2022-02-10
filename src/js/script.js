a35727f6ffe95ef72a6ef0e30040722fde4a7d16
Nika <vva12369@gmail.com>
2022-02-10 19:12:46 +0600

    js homework1

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,21 @@
-'esversion: 6';
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-const userInfo = {
-    name: "Вася",
-    age: 30,
-    "58": 'puk'
-}
-console.log(userInfo[58]);
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
