8b4b84c597b448cb456b7a0de63ca249c11e5f8f
Nika <vva12369@gmail.com>
2022-02-10 17:56:51 +0600

    again

--- src/js/script.js
+++ src/js/script.js
@@ -21,15 +21,4 @@ const userInfo = {
     "58": 'puk'
 }
 console.log(userInfo[58]);
-const userInfo = {
-    name: "Вася",
-    age: 30,
-    "58": 'puk'
-}
-console.log(userInfo[58]);
-const userInfo = {
-    name: "Вася",
-    age: 30,
-    "58": 'puk'
-}
-console.log(userInfo[58]);
+
