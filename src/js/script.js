e21d6437cb41e35f0c59e557981975ce8938d8e1
Nika <vva12369@gmail.com>
2022-02-10 16:50:01 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+'esversion: 6';
+
+const userInfo = {
+    name: "Вася",
+    age: 30,
+    "58": 'puk'
+}
+console.log(userInfo[58]);
+
+
+const userInfo = {
+    name: "Вася",
+    age: 30,
+    "58": 'puk'
+}
+console.log(userInfo[58]);
